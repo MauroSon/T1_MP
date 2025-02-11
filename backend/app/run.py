23ab393531370ec8837701b2e139791684b8ee19
Caroline Bohadana Rodrigues Viana <232050975@aluno.unb.br>
2025-02-11 14:56:28 -0300

    Delete backend directory

--- backend/app/run.py
+++
@@ -1,7 +0,0 @@
-from factory import create_app
-
-app = create_app()
-
-if __name__ == "__main__":
-    # Roda a aplicação
-    app.run(debug=True)
--- backend/database/base_helper.py
+++
@@ -1,9 +0,0 @@
-import pymysql
-import pymysql.cursors
-
-# Helper parente que irá fornecera a conexão e o cursor com o banco de dados
-class BaseHelper:
-    def __init__(self, connection: pymysql.connections.Connection , cursor: pymysql.cursors.Cursor):
-        self.conn = connection
-        self.cursor = cursor
-
